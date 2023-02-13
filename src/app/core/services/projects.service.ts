e7b0058528b466e37dee27f0161e2e253e3bf7ac
Jhoan Zerpa <jz_22@msn.com>
2023-02-13 18:46:34 -0400

    crear proyecto y listar

---
+++ src/app/core/services/projects.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Project } from '../models/project.models';
+import { map } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { GlobalComponent } from "../../global-component";
+
+const API_URL_BACK = GlobalComponent.API_URL_BACK;
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': `Bearer ${localStorage.getItem('token')}` }),
+
+  };
+
+@Injectable({ providedIn: 'root' })
+export class ProjectsService {
+
+    project!: Project;
+
+    constructor(private http: HttpClient) { }
+    /***
+     * Get All Project
+     */
+    getAll() {
+        return this.http.get<Project[]>(`api/project`);
+    }
+
+    getTypes() {
+        return this.http.get(API_URL_BACK + 'project/types', httpOptions);
+    }
+    
+    get() {
+        return this.http.get(API_URL_BACK + 'project', httpOptions);
+    }
+
+    create(data: any): Observable<any> {
+        
+        return this.http.post(API_URL_BACK + 'project', {
+        "nombre": data.nombre,
+        "descripcion": data.descripcion,
+        "tipoProyectoId": data.tipoProyectoId
+      }, httpOptions);
+    
+    }
+
+    update(id: any, data: any): Observable<any> {
+        return this.http.put(API_URL_BACK + 'project/'+id, data);
+    }
+
+    delete(id: any): Observable<any> {
+        return this.http.delete(API_URL_BACK + 'project/'+id, httpOptions);
+    }
+}
--- src/app/pages/projects/create/create.component.ts
+++ src/app/pages/projects/create/create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ProjectsService } from '../../../core/services/projects.service';
 
 // Ck Editer
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
@@ -16,10 +19,14 @@ export class CreateComponent implements OnInit {
 
   // bread crumb items
   breadCrumbItems!: Array<{}>;
+  typeCreate: boolean = false;
+  createForm!: UntypedFormGroup;
+  nombreProyecto: any = '';
+  types: any = [];
 
   public Editor = ClassicEditor;
 
-  constructor() { }
+  constructor(private formBuilder: UntypedFormBuilder, private _router: Router, private projectsService: ProjectsService) { }
 
   ngOnInit(): void {
     /**
@@ -29,11 +36,67 @@ export class CreateComponent implements OnInit {
       { label: 'Proyectos' },
       { label: 'Crear Proyecto', active: true }
     ];
+
+    this.createForm = this.formBuilder.group({
+      nombre: ['', [Validators.required]]
+    });
+
+    this.getTypes();
   }
+  
+   // convenience getter for easy access to form fields
+   get f() { return this.createForm.controls; }
 
   /**
   * Multiple Default Select2
   */
    selectValue = ['Choice 1', 'Choice 2', 'Choice 3'];
+
+   siguiente(){
+    // stop here if form is invalid
+    if (this.createForm.invalid) {
+      return;
+    }
+
+    this.nombreProyecto = this.f['nombre'].value;
+
+    this.breadCrumbItems = [
+      { label: 'Proyectos' },
+      { label: 'Crear Proyecto' },
+      { label: 'Tipo', active: true }
+    ];
+    this.typeCreate = true;
+   }
+
+   getTypes(){
+    this.projectsService.getTypes().pipe().subscribe(
+      (data: any) => {
+        this.types = data.data;
+    },
+    (error: any) => {
+      //this.error = error ? error : '';
+      //this.toastService.show(error, { classname: 'bg-danger text-white', delay: 15000 });
+    });
+   }
+
+   saveProject(type: any){
+
+    const project: any = {
+      nombre: this.nombreProyecto,
+      descripcion: "descripcion de proyecto",
+      tipoProyectoId: type
+    };
+
+    this.projectsService.create(project).pipe().subscribe(
+      (data: any) => {
+        localStorage.setItem('toast', 'true');
+        this._router.navigate(['/projects']);
+    },
+    (error: any) => {
+      //this.error = error ? error : '';
+      //this.toastService.show(error, { classname: 'bg-danger text-white', delay: 15000 });
+    });
+    
+   }
 
 }
