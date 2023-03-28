1c1e95effb5efbee3195248bcdb27b8372617520
Jhoan Zerpa <jz_22@msn.com>
2023-03-28 09:23:21 -0400

    redirigir si esta completo

--- src/app/extraspages/steps/steps.component.ts
+++ src/app/extraspages/steps/steps.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params, RoutesRecognized } from '@angular/router';
+import { ProjectsService } from '../../core/services/projects.service';
 
 @Component({
   selector: 'app-steps',
@@ -14,12 +15,51 @@ export class StepsComponent implements OnInit {
 
   project_id: any = '';
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private _router: Router, private route: ActivatedRoute, private projectsService: ProjectsService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.project_id = params['id'];
     });
+  }
+
+  comenzar(){
+      this.showPreLoader();
+      
+      this.projectsService.getAnswerQuestion(6).pipe().subscribe(
+        (data: any) => {
+          const info: any = data.data;
+          
+              this._router.navigate(['/'+this.project_id+'/project-anality']);
+            
+          this.hidePreLoader(); 
+      },
+      (error: any) => {
+        
+        this._router.navigate(['/projects/'+this.project_id+'/step']);
+        
+        this.hidePreLoader();
+        //this.error = error ? error : '';
+        //this.toastService.show(error, { classname: 'bg-danger text-white', delay: 15000 });
+      });
+  }
+
+   // PreLoader
+   showPreLoader() {
+    var preloader = document.getElementById("preloader");
+    if (preloader) {
+        (document.getElementById("preloader") as HTMLElement).style.opacity = "0.8";
+        (document.getElementById("preloader") as HTMLElement).style.visibility = "visible";
+    }
+  }
+
+  // PreLoader
+  hidePreLoader() {
+    var preloader = document.getElementById("preloader");
+    if (preloader) {
+        (document.getElementById("preloader") as HTMLElement).style.opacity = "0";
+        (document.getElementById("preloader") as HTMLElement).style.visibility = "hidden";
+    }
   }
 
 }
