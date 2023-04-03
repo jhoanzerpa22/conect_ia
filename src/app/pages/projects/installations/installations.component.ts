8c70f64b6308327fe41a9b20fdb9238567b0f1a6
Jhoan Zerpa <jz_22@msn.com>
2023-04-03 11:57:01 -0400

    change icons table

--- src/app/pages/projects/installations/installations.component.ts
+++ src/app/pages/projects/installations/installations.component.ts
@@ -337,6 +337,23 @@ export class InstallationsComponent {
     this.InstallationDatas.forEach((x: { state: any; }) => x.state = ev.target.checked)
   }
 
+  isChecked(){
+    var checkboxes: any = document.getElementsByName('checkAll');
+    var result
+    var checkedVal: any[] = [];
+    for (var i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i].checked) {
+        result = checkboxes[i].value;
+        checkedVal.push(result);
+      }
+    }
+    if (checkedVal.length > 0) {
+      return true;
+    }else{
+      return false;
+    }
+  }
+
   /**
   * Confirmation mail model
   */
