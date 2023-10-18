764e2c8a6d67dd4c6b9798d293dc4492e4411483
Jhoan Zerpa <jz_22@msn.com>
2023-10-18 13:39:35 -0400

    ruta heroku

--- src/app/global-component.ts
+++ src/app/global-component.ts
@@ -3,18 +3,18 @@ export const GlobalComponent = {
     API_URL : 'https://api-node.themesbrand.website/',
     //API_URL_BACK : 'https://connect-ia-production.up.railway.app/api/v1/',
     //API_URL_BACK : 'https://connect-ia.onrender.com/api/v1/',
-    //API_URL_BACK : 'https://conect-ia-7fc34370c4c9.herokuapp.com/api/v1/',
+    API_URL_BACK : 'https://conect-ia-7fc34370c4c9.herokuapp.com/api/v1/',
     //API_URL_BACK : 'http://localhost:3000/api/v1/',
-    API_URL_BACK : 'http://3.15.30.193:3000/api/v1/',
+    //API_URL_BACK : 'http://3.15.30.193:3000/api/v1/',
     headerToken : {'Authorization': `Bearer ${localStorage.getItem('token')}`},
 
     // Auth Api
     //AUTH_API:"https://api-node.themesbrand.website/auth/",
     //AUTH_API:"https://connect-ia-production.up.railway.app/api/v1/auth/",
     //AUTH_API: "https://connect-ia.onrender.com/api/v1/auth/",
-    //AUTH_API : 'https://conect-ia-7fc34370c4c9.herokuapp.com/api/v1/auth/',
+    AUTH_API : 'https://conect-ia-7fc34370c4c9.herokuapp.com/api/v1/auth/',
     //AUTH_API:"http://localhost:3000/api/v1/auth/",
-    AUTH_API:"http://3.15.30.193:3000/api/v1/auth/",
+    //AUTH_API:"http://3.15.30.193:3000/api/v1/auth/",
     
     // Products Api
     product:'apps/product',
